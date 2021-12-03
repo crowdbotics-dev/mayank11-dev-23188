9664f4a3da6658ca3513ea47be2a010e341961c1
Crowdbotics <engineering@crowdbotics.com>
2021-12-03 17:38:44 +0000

    Update models

--- backend/mayank12_dev_23188/wsgi.py
+++ backend/mayank12_dev_23188/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mayank12_dev_23188.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mayank12_dev_23188.settings")
 
 application = get_wsgi_application()
