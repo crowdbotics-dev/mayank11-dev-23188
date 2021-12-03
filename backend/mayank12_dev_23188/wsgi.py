6bb110c95800c6c3ed4778df0f8f0d81ad447ff9
Crowdbotics <engineering@crowdbotics.com>
2021-12-03 17:29:48 +0000

    Initial commit.

---
+++ backend/mayank12_dev_23188/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mayank12_dev_23188 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mayank12_dev_23188.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "mayank12_dev_23188",
+  url: "https://mayank12_dev_23188.botics.co",
+  api: "https://mayank12_dev_23188.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
