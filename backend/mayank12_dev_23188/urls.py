c7e31fc6bd9732140a5dc661fbf9e2f90e66de23
Crowdbotics <engineering@crowdbotics.com>
2021-12-03 17:32:12 +0000

    Update admin title

--- backend/mayank12_dev_23188/urls.py
+++ backend/mayank12_dev_23188/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "mayank12"
-admin.site.site_title = "mayank12 Admin Portal"
-admin.site.index_title = "mayank12 Admin"
+admin.site.site_header = "mayank11"
+admin.site.site_title = "mayank11 Admin Portal"
+admin.site.index_title = "mayank11 Admin"
 
 # swagger
 api_info = openapi.Info(
